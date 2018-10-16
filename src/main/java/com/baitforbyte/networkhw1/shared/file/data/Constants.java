c4598a6f239da01675af4586ae79b3e1d705fe84
Caner Korkmaz <info@canerkorkmaz.com>
2018-10-16 21:33:57 +0300

    Marked logs hidden

--- src/main/java/com/baitforbyte/networkhw1/shared/file/data/Constants.java
+++ src/main/java/com/baitforbyte/networkhw1/shared/file/data/Constants.java
@@ -2,8 +2,8 @@ package com.baitforbyte.networkhw1.shared.file.data;
 
 public final class Constants {
 
-    public static final String PREV_FILES_LOG_NAME = "prev.veryspeciallog";
-    public static final String CHANGE_FILES_LOG_NAME = "change.veryspeciallog";
+    public static final String PREV_FILES_LOG_NAME = ".prev.veryspeciallog";
+    public static final String CHANGE_FILES_LOG_NAME = ".change.veryspeciallog";
 
     private Constants() {
     }
